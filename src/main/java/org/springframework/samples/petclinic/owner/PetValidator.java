8db39f4f78b496a0bf56e2200b6f5d65ba2d1ac3
Zach Hudson <zhudson@eacpds.com>
2019-12-03 20:07:01 -0600

    First Set of Changes

--- src/main/java/org/springframework/samples/petclinic/owner/PetValidator.java
+++ src/main/java/org/springframework/samples/petclinic/owner/PetValidator.java
@@ -48,7 +48,7 @@ public class PetValidator implements Validator {
 
         // birth date validation
         if (pet.getBirthDate() == null) {
-            errors.rejectValue("birthDate", REQUIRED, REQUIRED);
+            //errors.rejectValue("birthDate", REQUIRED, REQUIRED);
         }
     }
 
